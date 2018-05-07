65fdf8690ffe2ab681d064de12ed96ae5ae711f9
tkoar <terrancekoar@gmail.com>
2018-05-07 17:37:13 -0400

    adding solution -- needs to be updated and needs tests to be updated

--- person.py
+++ person.py
@@ -1,4 +1,24 @@
-# import your Query class here
+# import car class here
+from car import Car
 
 class Person:
-    pass
+
+    def __init__(self, name, occupation):
+        self._name = name
+        self._occupation = occupation
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def occupation(self):
+        return self._occupation
+
+    @classmethod
+    def drives_a(cls, make):
+        cars = list(filter(lambda car.owner: car.make == make, Car.all()))
+        return cars
+
+    def drives_same_make_as_me(self):
+        pass
